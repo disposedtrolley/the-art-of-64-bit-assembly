5bead9219a2e55887a25f4f85653bd937ee9613c
James Liu <james@jamesliu.io>
2022-10-22 16:36:25 +1100

    Hello world

---
+++ hello-world/programShell.asm
@@ -0,0 +1,13 @@
+;; The code directive indicates that the statements following should
+;; go ijnto the section of memory reserved for machine instructions. 
+      .code
+
+;; PROC and ENP define the scope of the main function.
+main  PROC
+
+      ret
+
+main  ENDP
+
+;; END marks the end of the source file.
+      END
